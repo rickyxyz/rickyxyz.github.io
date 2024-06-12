3832f098c3b2731ca37c414c973e67579a38a988
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2024-06-12 19:16:47 +0700

    add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://rickyxyz.github.io",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), partytown({
+    config: {
+      debug: false,
+      forward: ['dataLayer.push']
+    }
+  })]
 });
