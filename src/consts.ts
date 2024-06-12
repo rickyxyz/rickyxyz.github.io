5a4169ad06d36e0da2a5ac001183e75cefc61456
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2024-06-12 15:43:56 +0700

    change site description

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "rickyxyz";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "rickyxyz's site";
+export const SITE_DESCRIPTION =
+  "Welcome to my website! Where I write all kind of stuff that came across my mind.";
