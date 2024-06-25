db18f3dea7298e2fe6c8029126957bad5cf07b9d
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2024-06-25 10:40:09 +0700

    feat: add project carousel

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,28 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+  }),
 });
 
-export const collections = { blog };
+const project = defineCollection({
+  type: "data",
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    pubDate: z.coerce.date(),
+    carouselImages: z.array(z.string()),
+    githubLink: z.string(),
+    liveLink: z.string(),
+  }),
+});
+
+export const collections = { blog, project };
