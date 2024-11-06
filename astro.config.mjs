e3bfd98c7142cf1e23db44c765f61f76a0339528
rickyxyz <56119701+rickyxyz@users.noreply.github.com>
2024-11-06 08:08:46 +0700

    feat: remove sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
 
 import preact from "@astrojs/preact";
@@ -8,15 +7,14 @@ import preact from "@astrojs/preact";
 // https://astro.build/config
 export default defineConfig({
   site: "https://rickyxyz.dev",
-  integrations: [mdx(), sitemap(), partytown({
-    config: {
-      debug: false,
-      forward: ['dataLayer.push', 'gtag']
-    }
-  }), preact(),
-  sitemap({
-    filter: (page) => page !== 'https://rickyxyz.dev/frontendmentor-projects/',
-    entryLimit: 1000,
-    changefreq: 'weekly',
-  })]
+  integrations: [
+    mdx(),
+    partytown({
+      config: {
+        debug: false,
+        forward: ["dataLayer.push", "gtag"],
+      },
+    }),
+    preact(),
+  ],
 });
